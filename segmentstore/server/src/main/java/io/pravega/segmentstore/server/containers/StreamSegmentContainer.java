5639fadb300a6862a81354ad16f20ac910614600
AndreyKoltsov1997 <a99920641@gmail.com>
2020-03-11 17:39:48 +0300

    Add logs in order to try match segment container with segment.

--- segmentstore/server/src/main/java/io/pravega/segmentstore/server/containers/StreamSegmentContainer.java
+++ segmentstore/server/src/main/java/io/pravega/segmentstore/server/containers/StreamSegmentContainer.java
@@ -354,6 +354,7 @@ class StreamSegmentContainer extends AbstractService implements SegmentContainer
         return this.metadataStore.getOrAssignSegmentId(streamSegmentName, timer.getRemaining(),
                 streamSegmentId -> {
                     val operation = new StreamSegmentAppendOperation(streamSegmentId, data, attributeUpdates);
+                    log.info("Append Request: stream segment ID: {}, Container ID: {}", streamSegmentId, this.metadata.getContainerId());
                     return processAppend(operation, timer).thenApply(v -> operation.getLastStreamSegmentOffset());
                 });
     }
@@ -368,6 +369,7 @@ class StreamSegmentContainer extends AbstractService implements SegmentContainer
         return this.metadataStore.getOrAssignSegmentId(streamSegmentName, timer.getRemaining(),
                 streamSegmentId -> {
                     val operation = new StreamSegmentAppendOperation(streamSegmentId, offset, data, attributeUpdates);
+                    log.info("Append Request with offset: stream segment ID: {}, Container ID: {}", streamSegmentId, this.metadata.getContainerId());
                     return processAppend(operation, timer).thenApply(v -> operation.getLastStreamSegmentOffset());
                 });
     }
